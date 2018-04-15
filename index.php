d9d812fa88f998b7cd68de964404f2a22fc6a0cc
Edrine <edrine.ssemwanga@ashesi.edu.gh>
2018-04-15 23:03:01 +0000

    updated images

--- index.php
+++ index.php
@@ -118,12 +118,12 @@
 </div>
 </div>
 <!-- Header End====================================================================== -->
-<div id="carouselBlk">
+<div id="carouselBlk" style="width:100%; height=10%">
 	<div id="myCarousel" class="carousel slide">
-		<div class="carousel-inner">
+		<div class="carousel-inner" >
 		  <div class="item active">
-		  <div class="container">
-			<a href="view/register.php"><img style="width:100%" src="themes/images/carousel/1.png" alt="special offers"/></a>
+		  <div class="container" style="width:100%;height=10%" >
+			<a href="view/register.php"><img src="themes/images/carousel/img81.png" alt="special offers"/></a>
 			<div class="carousel-caption">
 				  <h4>Second Thumbnail label</h4>
 				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
@@ -131,8 +131,8 @@
 		  </div>
 		  </div>
 		  <div class="item">
-		  <div class="container">
-			<a href="view/register.php"><img style="width:100%" src="themes/images/carousel/2.png" alt=""/></a>
+		  <div class="container" >
+			<a href="view/register.php"><img  src="themes/images/carousel/img1.png" alt=""/></a>
 				<div class="carousel-caption">
 				  <h4>Second Thumbnail label</h4>
 				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
@@ -140,8 +140,8 @@
 		  </div>
 		  </div>
 		  <div class="item">
-		  <div class="container">
-			<a href="view/register.php"><img src="themes/images/carousel/3.png" alt=""/></a>
+		  <div class="container" style="width:100%; height=10%">
+			<a href="view/register.php"><img  src="themes/images/carousel/img5.jpg" alt=""/></a>
 			<div class="carousel-caption">
 				  <h4>Second Thumbnail label</h4>
 				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
@@ -150,8 +150,8 @@
 		  </div>
 		  </div>
 		   <div class="item">
-		   <div class="container">
-			<a href="view/register.php"><img src="themes/images/carousel/4.png" alt=""/></a>
+		   <div class="container" style="width:100%; height=10%">
+			<a href="view/register.php"><img src="themes/images/carousel/img11.png" alt=""/></a>
 			<div class="carousel-caption">
 				  <h4>Second Thumbnail label</h4>
 				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
@@ -159,7 +159,7 @@
 		   
 		  </div>
 		  </div>
-		   <div class="item">
+		   <!-- <div class="item">
 		   <div class="container">
 			<a href="view/register.php"><img src="themes/images/carousel/5.png" alt=""/></a>
 			<div class="carousel-caption">
@@ -167,8 +167,8 @@
 				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 			</div>
 		  </div>
-		  </div>
-		   <div class="item">
+		  </div> -->
+		   <!-- <div class="item">
 		   <div class="container">
 			<a href="view/register.php"><img src="themes/images/carousel/6.png" alt=""/></a>
 			<div class="carousel-caption">
@@ -176,7 +176,7 @@
 				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 				</div>
 		  </div>
-		  </div>
+		  </div> -->
 		</div>
 		<a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
 		<a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
